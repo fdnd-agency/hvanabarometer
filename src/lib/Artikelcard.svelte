cd7e4a5eb97b4598362e9f5f1522d6ad1dcaff54
Mohamed <mohamedelibrahymy374@gmail.com>
2026-09-24 18:10:28 +0200

    #15 #wontdo toegevoegd

--- src/lib/Artikelcard.svelte
+++ src/lib/Artikelcard.svelte
@@ -1,7 +1,6 @@
 <section>
   <article class="cards">
-    <a class="card" href="#">
-      <img src="https://picsum.photos/id/1005/600/400" alt="" loading="lazy" />
+    <a class="card" href="#WONTDO">
       <span class="tag">Column</span>
       <h3>
         ‘We laten studenten samenwerken zonder ze ooit te leren hoe je dat doet’
