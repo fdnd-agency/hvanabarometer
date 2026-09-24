2db7f6cc32ec790c0471959ecf78684435a5d6e3
Mohamed <mohamedelibrahymy374@gmail.com>
2026-09-24 15:41:40 +0200

    #15 Artikel component HTML first

---
+++ src/lib/Artikel.svelte
@@ -0,0 +1,12 @@
+<script>
+  import ArtikelCard from "$lib/Artikelcard.svelte";
+</script>
+
+<section>
+  <ul class="articlebox">
+    <ArtikelCard />
+    <ArtikelCard />
+    <ArtikelCard />
+  </ul>
+  <a class="clickmorebtn" href="#">Meer columns</a>
+</section>
