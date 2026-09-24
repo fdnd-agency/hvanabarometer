ff1c6e7872b2b34f1b680e758cd8c201e3383b32
Mohamed <mohamedelibrahymy374@gmail.com>
2026-09-24 15:41:51 +0200

    #15 Artikel component styling

--- src/lib/Artikel.svelte
+++ src/lib/Artikel.svelte
@@ -10,3 +10,35 @@
   </ul>
   <a class="clickmorebtn" href="#">Meer columns</a>
 </section>
+
+<style>
+  section {
+    background: #ff5a2e;
+    padding-bottom: 3rem;
+  }
+  .articlebox {
+    list-style: none;
+    justify-content: center;
+    align-items: center;
+    padding: 4rem 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+
+    @media (min-width: 1000px) {
+      flex-direction: row;
+    }
+  }
+  .clickmorebtn {
+    display: block;
+    width: fit-content;
+    margin: 0 auto;
+    padding: 16px 40px;
+    background: white;
+    color: black;
+    border-radius: 4px;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-weight: bold;
+  }
+</style>
