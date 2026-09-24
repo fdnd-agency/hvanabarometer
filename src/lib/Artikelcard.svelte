1ac3f5bdd05c43243ef205f33a44caf9235ba41d
Mohamed <mohamedelibrahymy374@gmail.com>
2026-09-24 15:40:50 +0200

    #15 HTML FIRST

---
+++ src/lib/Artikelcard.svelte
@@ -0,0 +1,11 @@
+<section>
+  <article class="cards">
+    <a class="card" href="#">
+      <img src="https://picsum.photos/id/1005/600/400" alt="" />
+      <span class="tag">Column</span>
+      <h3>
+        ‘We laten studenten samenwerken zonder ze ooit te leren hoe je dat doet’
+      </h3>
+    </a>
+  </article>
+</section>
