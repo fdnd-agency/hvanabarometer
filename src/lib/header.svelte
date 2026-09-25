f6a1bd7edc747aadac62bf7beb51046fb31742b8
Angelaaliyah12 <angel.blom@hva.nl>
2026-09-25 11:26:35 +0200

    #17 content first

---
+++ src/lib/header.svelte
@@ -0,0 +1,35 @@
+<p>Nieuws</p>
+<p>Video</p>
+<p>Columns</p>
+<p>Opinie</p>
+<p>Podcast</p>
+
+<img class="hvana-logo" width="667" height="202" alt="HvanA" 
+src="https://hvana.nl/wp-content/uploads/2023/03/hvana_logo_wit-2025.svg" 
+data-orig-src="https://hvana.nl/wp-content/uploads/2023/03/hvana_logo_wit-2025.svg" >
+
+<div class="wrapper">
+    <a class="twitter-1" style="color:var(--awb-color1);font-size:24px;" 
+    title="X" aria-label="twitter" target="_blank" rel="noopener noreferrer" 
+    href="https://twitter.com/HvanA_nl">
+<img class="twitter-img" src="/assets/twitter.png"/>
+</a>
+    <a class="instagram" style="color:var(--awb-color1);font-size:24px;" 
+    title="Instagram" aria-label="instagram" target="_blank" rel="noopener noreferrer" 
+    href="https://www.instagram.com/hvana_nl/">
+    <img class="insta-img" src="/assets/instagram.png" alt="instagram-icon"/>
+    </a>
+    <a class="youtube" style="color:var(--awb-color1);font-size:24px;" 
+    title="YouTube" aria-label="youtube" target="_blank" rel="noopener noreferrer" 
+    href="https://www.youtube.com/c/HvanA">
+
+    <img class="yt-img" src="/assets/youtube.png" />
+    </a>
+    <a class="icon-linkedin" style="color:var(--awb-color1);font-size:24px;"
+     title="LinkedIn" aria-label="linkedin" target="_blank" rel="noopener noreferrer" 
+     href="https://nl.linkedin.com/company/hvana">
+     <img class="linkedin-img" src="/assets/linkedin.png"/>
+     </a>
+</div>
+<img class="search-img" src="/assets/search.svg"/>
+<img class="menu-img" src="/assets/menu.png"/>
